116583a32a58fe18132fc1770fa9f007c41018d9
Pragna P <pragna.p@happiestminds.com>
2022-11-21 12:23:10 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  template: `Menu <ul>
+    <li> <a [routerLink] ="['/Home']"> Home</a></li>
+  <li> <a [routerLink] ="['/Apply']"> Apply Application</a></li>
+  <li> <a [routerLink] ="['/View']"> View Application</a></li>
+  <li> <a [routerLink] ="['/Query']"> Query</a></li>
+  <li> <a [routerLink] ="['/ContactUs']"> Contact Us</a></li>
+  <li> <a [routerLink] ="['/Feedback']"> Feedback</a></li>
+  </ul><hr><hr>
+  <router-outlet></router-outlet>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ApplyComponent } from './apply/apply.component';
+import { ViewComponent } from './view/view.component';
+import { RouterModule , Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { QueryComponent } from './query/query.component';
+import { ContactComponent } from './contact/contact.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+const appRoutes: Routes =[		
+  {path:'Apply' , component:ApplyComponent},
+  {path:'View', component:ViewComponent},
+  {path:'Home' , component:HomeComponent},
+  {path:'Query' , component:QueryComponent},
+  {path:'Feedback' , component:FeedbackComponent},
+  {path:'ContactUs' , component:ContactComponent},
+  ];
+  
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ApplyComponent,
+    ViewComponent,
+    HomeComponent,
+    QueryComponent,
+    ContactComponent,
+    FeedbackComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(appRoutes),
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ HomeComponent]
 })
 export class AppModule { }
